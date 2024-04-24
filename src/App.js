3c6bd8096e2f025851f7f1cde76577ce1327a8f6
Orikku <138827748+Orikku@users.noreply.github.com>
2024-04-23 22:29:45 -0500

    Banner

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import React from 'react';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>WasteWiseMap</h1>
+      <header className="Banner">
+        <h1>WasteWiseMap</h1>
+        <nav>
+          <ul>
+            <li><a href="#inicio">Inicio</a></li>
+            <li><a href="#acerca">Acerca</a></li>
+            <li><a href="#mapa">Mapa</a></li>
+            <li><a href="#contacto">Contacto</a></li>
+          </ul>
+        </nav>
+      </header>
     </div>
   );
 }
