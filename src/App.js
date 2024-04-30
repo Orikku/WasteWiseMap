815d79c52ce78dbfd42c7211873ddff8a5a9676b
SebasMR0710 <sebastianmongui32@gmail.com>
2024-04-30 14:57:22 -0500

    Actualización mapa de reciclaje
    Se actualizo el mapa para que muestre diferentes ubicaciones con puntos de reciclaje, en esto caso de plástico

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
         <h2>Puntos de recolección</h2>
         <iframe 
           title="Mapa de Bogotá"
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15984.175050867383!2d-74.07927175804522!3d4.656050887600506!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f9a61b2287e15%3A0x2d5a8e6c5d537231!2sAv.%2070%20%2353-40%2C%20Bogot%C3%A1%2C%20Colombia!5e0!3m2!1sen!2sus!4v1648510482867!5m2!1sen!2sus"
+          src="https://www.google.com/maps/d/embed?mid=1rtUGGB9MJNFbMfbmrXEPZ_uibgtuSP4&ehbc=2E312F&noprof=1"
           width="100%"
           height="600"
           style={{ border: 0, marginTop: "25px" }}
